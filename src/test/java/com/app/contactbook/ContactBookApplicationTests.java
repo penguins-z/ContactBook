cd82cd74c0b65604e92920f7ae7a7cc04bc27d64
penguins-z <ferdinrodriguez005@gmail.com>
2025-06-25 23:36:35 +0530

    Initial commit

---
+++ src/test/java/com/app/contactbook/ContactBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.contactbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
